d93aa66e2a64c3a9418c9ed3618497f6f654c089
DESKTOP-6UFBIJ2\simpl <ppark9553@gmail.com>
2020-12-29 12:17:56 +0900

    apply rabbitmq again

--- handlers/kiwoom_handler.py
+++ handlers/kiwoom_handler.py
@@ -9,8 +9,10 @@ sio.connect('http://localhost:3001')
 
 @sio.on('kiwoom_stocks')
 def on_receive_kiwoom_stocks_data(data):
-    data = data
+    data = json.loads(data)
+    print(data.get('trade_date'), data.get('timestamp'), data.get('hoga_date'))
 
 @sio.on('kiwoom_futures')
 def on_receive_kiwoom_futures_data(data):
-    data = data
+    data = json.loads(data)
+    print(data.get('trade_date'), data.get('timestamp'), data.get('hoga_date'))
